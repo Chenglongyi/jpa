299ffad8498018de8348ab9d65552ba4f96d156c
chenglongyi <chenglongyiyiyi@126.com>
2018-07-16 17:05:26 +0800

    初始架构

---
+++ src/main/java/com/example/jpa/JpaApplication.java
@@ -0,0 +1,18 @@
+package com.example.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaApplication.class, args);
+	}
+
+
+	public String hello(){
+		return "This is a test";
+	}
+}
